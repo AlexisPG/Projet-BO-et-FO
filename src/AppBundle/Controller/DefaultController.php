5dbdfb1efad0ff3756348120caab6e018c9da6b8
Your Name <you@example.com>
2017-01-04 12:42:18 +0100

    Sauvegarde apres avoir créé le BO : admin + page produits

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+      $firstname = "Alexis";
+        // replace this example code with whatever you need
+        return $this->render('default-old//index.html.twig', [
+          'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+          "firstname" => $firstname
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+
+    public function contactAction(Request $request)
+    {
+      $firstname = "Alexis";
+      // dump($firstname);
+      // die("end");
+      return $this->render('default-old//contact.html.twig', [
+        "firstname" => $firstname
+          ]);
+
+    }
+
+        /**
+     * @Route("/product", name="product-test")
+     */
+
+    public function productAction(Request $request)
+    {
+        $products = [
+    [
+        "id" => 1,
+        "title" => "mon premier produit",
+        "description" => "lorem ipsum",
+        "date_created" => new \DateTime('now'),
+        "prix" => 10
+    ],
+    [
+        "id" => 2,
+        "title" => "Mon deuxième produit",
+        "description" => "lorem ipsum",
+        "date_created" => new \DateTime('now'),
+        "prix" => 20
+    ],
+    [
+        "id" => 3,
+        "title" => "Mon troisième produit",
+        "description" => "lorem ipsum",
+        "date_created" => new \DateTime('now'),
+        "prix" => 30
+    ],
+    [
+        "id" => 4,
+        "title" => "",
+        "description" => "lorem ipsum",
+        "date_created" => new \DateTime('now'),
+        "prix" => 410
+    ],
+    ];
+    $prenom = "Alexis";
+    $nom = "Persinette";
+
+      return $this->render('default/product.html.twig', [
+        "products" => $products,
+        "prenom" => $prenom,
+        "nom" => $nom
+
+          ]);
+    }
+
+    /**
+     * @Route("/bloc-mere", name="bloc-mere")
+     */
+
+    public function blocMereAction(Request $request)
+    {
+
+      return $this->render('default-old//bloc-mere.html.twig', [
+          ]);
+
+    }
+
+    /**
+     * @Route("/bloc-fille", name="bloc-fille")
+     */
+
+    public function blocFilleAction(Request $request)
+    {
+
+      return $this->render('default-old//bloc-fille.html.twig', [
+          ]);
+
+    }
+    /**
+     * @Route("/bloc-frere", name="bloc-frere")
+     */
+
+    public function blocFrereAction(Request $request)
+    {
+
+      return $this->render('default-old//bloc-frere.html.twig', [
+          ]);
+
+    }
+}
